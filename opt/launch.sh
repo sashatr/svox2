feeb1d5b192e193ce1d8edcd8efc8ec53e5fa3a2
Oleksandr Trofymenko <trofymenko.oleks@gmail.com>
2022-04-17 13:48:20 +0300

    run launch script without nohup

--- opt/launch.sh
+++ opt/launch.sh
@@ -10,5 +10,11 @@ NOHUP_FILE=$CKPT_DIR/log
 echo CKPT $CKPT_DIR
 echo LOGFILE $NOHUP_FILE
 
-CUDA_VISIBLE_DEVICES=$2 nohup python -u opt.py -t $CKPT_DIR ${@:3} > $NOHUP_FILE 2>&1 &
-echo DETACH
+CUDA_VISIBLE_DEVICES=$2
+
+# CUDA_VISIBLE_DEVICES=$2 nohup python3 -u opt.py -t $CKPT_DIR ${@:3} > $NOHUP_FILE 2>&1 &
+# echo DETACH
+python3 -u opt.py -t $CKPT_DIR ${@:3}
+
+# ./launch.sh <exp_name> <GPU_id> <data_dir> -c <config>
+# ./launch.sh chair_test 0 ../nerf_synthetic/chair/ -c configs/syn.json
